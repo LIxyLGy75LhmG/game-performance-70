16b7aa3f91dd401e6740646d351392139cbfaae6
Lukas Schmidt <sun.play@hotmail.com>
2026-06-12 08:10:22 +0200

    refactor logging functionality

--- logger.py
+++ logger.py
@@ -1,20 +1,38 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='game.log', max_bytes=5 * 1024 * 1024, backup_count=3):
-    logger = logging.getLogger('game_logger')
-    logger.setLevel(logging.DEBUG)
+class GameLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(f'{name}.log')
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    def debug(self, message):
+        self.logger.debug(message)
 
-    if not logger.hasHandlers():
-        logger.addHandler(handler)
-    return logger
+    def info(self, message):
+        self.logger.info(message)
 
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+    @staticmethod
+    def set_global_level(level):
+        root_logger = logging.getLogger()
+        root_logger.setLevel(level)
+
+# Usage Example
 if __name__ == '__main__':
-    log = setup_logger()
-    log.info('Logger initialized with rotation')
-    log.debug('This is a debug message')
-    log.error('This is an error message')
+    logger = GameLogger('MyGame')
+    logger.info('Game started')
+    logger.warning('Low health warning')
+    logger.error('Error loading level')
