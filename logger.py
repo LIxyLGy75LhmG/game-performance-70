ec6951e663a9a06ce3db18ce4e720727749f045b
Lukas Schmidt <sun.play@hotmail.com>
2026-07-14 11:09:55 +0500

    Add implement custom logging functionality

--- logger.py
+++ logger.py
@@ -1,39 +1,29 @@
 import logging
-import os
 
-class GameLogger:
+class CustomLogger:
     def __init__(self, name):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler(f'logs/{name}.log')
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-    def log_event(self, event):
-        self.logger.info(event)
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    def log_error(self, error):
-        self.logger.error(error)
+    def info(self, msg):
+        self.logger.info(msg)
 
-    def log_warning(self, warning):
-        self.logger.warning(warning)
+    def warning(self, msg):
+        self.logger.warning(msg)
 
-    def close(self):
-        for handler in self.logger.handlers:
-            handler.close()
-            self.logger.removeHandler(handler)
+    def error(self, msg):
+        self.logger.error(msg)
 
-# Example usage
-if __name__ == '__main__':
-    os.makedirs('logs', exist_ok=True)  
-    game_logger = GameLogger('game_session')
-    try:
-        game_logger.log_event('Game started')
-        # Game logic here
-        raise ValueError('An example error')
-    except Exception as e:
-        game_logger.log_error(str(e))
-    finally:
-        game_logger.log_event('Game ended')
-        game_logger.close()
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+custom_logger = CustomLogger('game_logger')
+custom_logger.info('Logger initialized for game performance')
