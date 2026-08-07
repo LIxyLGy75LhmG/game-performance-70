bfbad4bf28d3c983eb40da890597289f72e84655
Lukas Schmidt <sun.play@hotmail.com>
2026-08-07 11:11:09 +0500

    Update structured logging implementation

--- logger.py
+++ logger.py
@@ -1,37 +1,35 @@
 import logging
 
-class GameLogger:
-    def __init__(self, log_file='game.log'):
-        logging.basicConfig(filename=log_file, level=logging.DEBUG,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-    def log_info(self, message):
-        logging.info(message)
+    def debug(self, msg):
+        self.logger.debug(msg)
 
-    def log_warning(self, message):
-        logging.warning(message)
+    def info(self, msg):
+        self.logger.info(msg)
 
-    def log_error(self, message):
-        logging.error(message)
+    def warning(self, msg):
+        self.logger.warning(msg)
 
-    def log_debug(self, message):
-        logging.debug(message)
+    def error(self, msg):
+        self.logger.error(msg)
 
-    def validate_input(self, user_input):
-        if not isinstance(user_input, str) or not user_input:
-            self.log_error('Invalid input received')
-            return False
-        return True
+    def critical(self, msg):
+        self.logger.critical(msg)
 
-    def process_input(self, user_input):
-        if self.validate_input(user_input):
-            self.log_info(f'Processing input: {user_input}')
-            # Process the input here
-            return True
-        return False
-
+# Example usage
 if __name__ == '__main__':
-    logger = GameLogger()
-    sample_inputs = ['', 'valid_input', 123]
-    for input_value in sample_inputs:
-        logger.process_input(input_value)
+    log = Logger('MyGameLogger')
+    log.info('Game started')
+    log.debug('Debugging info')
+    log.warning('This is a warning')
+    log.error('An error occurred')
+    log.critical('Critical issue')
